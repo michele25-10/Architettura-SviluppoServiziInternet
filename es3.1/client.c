336fcd8760a6f184e4cc65367f90d6f5c30574c9
Michele Gabrieli <michele.gabri01@gmail.com>
2025-11-20 17:07:05 +0100

    Nuova struttura repo + Esercitazione 3

---
+++ es3.1/client.c
@@ -0,0 +1,11 @@
+#define _POSIX_C_SOURCE 200809L
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <signal.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include "utils.h"
